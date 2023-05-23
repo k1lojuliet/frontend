1b47d611184a1b46c823deb161e2e03f9a89fe49
k1lojuliet <karenjil.bagol@carsu.edu.ph>
2023-05-24 00:22:12 +0800

    Added Axios Functionality to acess OpenAi

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const { app, BrowserWindow } = require("electron");
+// Imported Modules
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+const axios = require('axios');
 
+// Main Window
 const isDev = true;
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: isDev ? 1200: 600,
+    width: isDev ? 1200 : 600,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
@@ -20,6 +23,9 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
+  // Initialize Functions
+  ipcMain.handle('axios.openAI', openAI);
+
   createWindow();
 
   app.on("activate", () => {
@@ -34,3 +40,33 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
+
+// Main Functions
+async function openAI(event, sentence){
+  let result = null;
+
+  await axios({
+     method: 'post',
+     url: 'https://api.openai.com/v1/completions',
+     data: {
+       model: "text-davinci-003",
+       prompt: "Correct this to standard English:\n\n" + sentence,
+       temperature: 0,
+       max_tokens: 60,
+       top_p: 1.0,
+       frequency_penalty: 0.0,
+       presence_penalty: 0.0
+     },
+     headers: {
+       'Content-Type': 'application/json',
+       'Authorization': 'Bearer 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'
+     }
+   }).then(function (response) {
+     res = response.data;
+   })
+   .catch(function (error) {
+     res = error;
+  });
+
+  return res;
+}
