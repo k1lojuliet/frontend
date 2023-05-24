c5308cd94229a7caea86ae335bc4bf65fd7b413b
k1lojuliet <karenjil.bagol@carsu.edu.ph>
2023-05-24 09:42:26 +0800

    Added Toastify Alerts

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -7,27 +7,34 @@ if (form) {
     const formData = new FormData(form);
     const sentence = formData.get("sentence");
 
-    window.Toastify.showToast({
-        text: "This is a toast",
-        duration: 3000,
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-            textAllign: "center",
-            background: "pink", 
-            color: "white",
-            padding: "5px",
-            marginTop: "2px"
-            
-        },
-      });
+    if (sentence.length <= 5) {
+      alertMessages("success", "Please input at least 8 characters");
 
-//     try {
-//       const response = await window.axios.openAI(sentence);
+      return;
+    }
 
-//       document.getElementById("sentence_corrected").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
-//     } catch (error) {
-//       console.error("Error:", error);
-//       // Handle or display the error message appropriately
-//     }
-//   };
+    try {
+      const response = await window.axios.openAI(sentence);
+
+      document.getElementById("sentence_corrected").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle or display the error message appropriately
+    }
+  };
+}
+
+function alertMessages(status, sentence) {
+  window.Toastify.showToast({
+    text: sentence,
+    duration: 3000,
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      textAlign: "center",
+      background: status=="error" ? "pink":"blue",
+      color: "white",
+      padding: "5px",
+      marginTop: "2px",
+    },
+  });
 }
